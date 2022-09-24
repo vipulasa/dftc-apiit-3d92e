1585115a6dbf53a79a8e2520aff982fabb74af11
Vipula S Anandapiya <v.s.anandapiya@gmail.com>
2022-09-24 12:12:49 +0530

    mail trap email sending feature added

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,34 @@
-// const functions = require("firebase-functions");
+const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
 
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello DFTC APIIT!");
-// });
+exports.sendContactEmail = functions.https.onRequest((request, response) => {
+  // functions.logger.info("Hello logs!", {structuredData: true});
+  // response.send("Hello DFTC APIIT!");
+
+  const nodemailer = require("nodemailer");
+  const transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "f35dda427f3e72",
+      pass: "3ba77f94c543b5",
+    },
+  });
+
+  const mailOptions = {
+    from: request.body.email,
+    to: "test@test.com",
+    subject: "New Message from " + request.body.name,
+    html: `<h1>${request.body.name}</h1><p>${request.body.message}</p>`,
+  };
+
+  transport.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
+});
